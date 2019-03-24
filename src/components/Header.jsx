d3e2f946b3d6cb56ab90fa264c0e13ec8203cba0
FeronCastor <ferocastor@gmail.com>
2019-03-25 00:41:10 +0700

    project start

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fpt%2Fthumb%2F7%2F75%2FSparta_Prague_logo.png%2F200px-Sparta_Prague_logo.png&f=1' alt=''/>
+		</header>
+	);
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+		<div>
+			<img src='https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fthewallpaper.co%2Fwp-content%2Fuploads%2F2016%2F01%2Fwidescreen-images-desktop-images-colors-free-images-download-wallpapers-large-images-cool-1200x800.jpg&f=1' alt=''/>
+		</div>
+		<div>
+			<img src='https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1470900358%2Ftanuki_200-200_400x400.png&f=1' alt='' />
+		</div>
+		<div>
+			My posts
+			<div>
+				New post
+			</div>
+			<div>
+				post 1
+			</div>
+			<div>
+				post 2
+			</div>
+		</div>
+	</div>
+	);
+}
+
+export default Profile;
